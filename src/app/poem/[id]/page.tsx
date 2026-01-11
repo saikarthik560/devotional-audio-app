3ef02407b4fa752beb5b9345981c4d6ad049c875
saikarthik560 <saikarthik5421@gmail.com>
2026-01-11 18:33:14 +0530

    Change Page component to handle async params

--- src/app/poem/[id]/page.tsx
+++ src/app/poem/[id]/page.tsx
@@ -8,6 +8,11 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <PoemPlayerClient id={params.id} />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  return <PoemPlayerClient id={id} />;
 }
